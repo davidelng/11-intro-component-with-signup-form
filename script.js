68baa3340444c64fc761119146ef4a1bf2010aac
Davide <davidelngyt@gmail.com>
2021-12-17 22:13:57 +0100

    initial styles

---
+++ script.js
@@ -0,0 +1,5 @@
+const firstName = document.querySelector('#first__name');
+const lastName = document.querySelector('#last__name');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const btn = document.querySelector('#btn');
